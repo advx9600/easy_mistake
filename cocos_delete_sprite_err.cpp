74dd0c9778ca5d226e3f1585b61be0c58cd53b4c
advx9600 <nwz88348307@163.com>
2014-05-24 10:20:06 +0800

    Update cocos_delete_sprite_err.cpp
    supplement

--- cocos_delete_sprite_err.cpp
+++ cocos_delete_sprite_err.cpp
@@ -1,13 +1,17 @@
 void init()
 {
 	vecTouchStar = new std::vector<Sprite*>();
-	vecTouchStar->push_back(Sprite::create("1.png"));
-	vecTouchStar->push_back(Sprite::create("2.png"));
+	auto s1=Sprite::create("1.png");
+	auto s2=Sprite::create("2.png");
+	vecTouchStar->push_back(s1);
+	vecTouchStar->push_back(s2);
+	this->addChild(s1);
+	this->addChild(s2);
 }
 
 void doclick()
 {
-	// when click is wrong
+	// when click is wrong, may be a bug
 	std::vector<Sprite*>::iterator begin = vecTouchStar->begin();
 	std::vector<Sprite*>::iterator end = vecTouchStar->end();	
 	for(begin;begin<end;begin++)
