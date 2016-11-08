ec4d94af8c4b54e225687a50fe1b0300e490c933
nwz <nwz88348307@163.com>
2016-11-08 10:36:01 +0800

    java too big

---
+++ java_too_big.java
@@ -0,0 +1,16 @@
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			long t = 1478571898 *1000;
+			Date date = new Date(t);
+			a.b("t        :"+t);
+			a.b("timestamp:"+date.getTime());
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			
+			a.b(simpleDateFormat.format(date));
+	}
+
+
+
+
+
+## long t = 1478571898l *1000;
