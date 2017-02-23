ac002cc1da48eae0d0058ead387cfda4cf009833
nwz <nwz88348307@163.com>
2017-02-23 16:15:19 +0800

    int double wrong

---
+++ java_int_double.java
@@ -0,0 +1,8 @@
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		a.b(""+test(2/3));
+	}
+
+	public static double test(double val){
+		return val*100;
+	}
